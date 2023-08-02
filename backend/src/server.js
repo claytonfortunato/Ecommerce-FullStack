24d63dfbe5a0ab8ae76c8d4179ee041aa4fe055d
claytonfortunato <claytonreis10@gmail.com>
2023-08-01 21:21:22 -0300

    feat: products api backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 
-const connectToDB = require("./src/config/db");
+const connectToDB = require("./config/db");
 const app = express();
 
 const port = 8000;
