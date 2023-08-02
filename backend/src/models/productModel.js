f65a1101c0b0daf3bc6d73f631485d9f69c19684
claytonfortunato <claytonreis10@gmail.com>
2023-08-01 21:35:46 -0300

    feat: add model backend

---
+++ backend/src/models/productModel.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+const Product = mongoose.model("product", ProductSchema);
+
+module.exports = Product;
