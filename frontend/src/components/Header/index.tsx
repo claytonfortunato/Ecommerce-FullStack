25b85d641a166b5b3bea92d48b3326c96db38d2e
claytonfortunato <claytonreis10@gmail.com>
2023-08-04 14:50:14 -0300

    feat: component header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <div>Ecommerce API</div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <span>Cart</span>
+        <span>Sign In</span>
+      </div>
+    </div>
+  );
+};
