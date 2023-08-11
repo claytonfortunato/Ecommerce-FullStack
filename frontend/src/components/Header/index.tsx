a108ebe76b02a5a33acf5ea50079709e0b0f1f4a
claytonfortunato <claytonreis10@gmail.com>
2023-08-11 15:43:21 -0300

    feat: added postcss and autoprefixer

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,14 @@
 export const Header = () => {
   return (
-    <div className="flex ">
-      <div>Ecommerce API</div>
+    <div className="flex text-center">
+      <div className="text-3xl font-bold">Ecommerce API</div>
       <div>
-        <label htmlFor="">Pesquisa</label>
-        <input type="text" className="border-[]" />
+        <label htmlFor="search">Pesquisa</label>
+        <input
+          type="text"
+          id="search"
+          className="bg-gray-50 border-2 hover:border-t-4 border-gray-300"
+        />
       </div>
       <div>
         <span>Cart</span>
