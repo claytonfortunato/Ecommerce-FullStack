38bfc18243f9caaf4be5e4cbdc4b03ae34b35f38
claytonfortunato <claytonreis10@gmail.com>
2023-08-13 13:30:07 -0300

    feat: config tailwindcss

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,14 +1,12 @@
 export const Header = () => {
   return (
-    <div className="flex text-center">
-      <div className="text-3xl font-bold">Ecommerce API</div>
+    <div className="">
+      <div className="text-3xl font-bold underline bg-slate-400">
+        Ecommerce API
+      </div>
       <div>
         <label htmlFor="search">Pesquisa</label>
-        <input
-          type="text"
-          id="search"
-          className="bg-gray-50 border-2 hover:border-t-4 border-gray-300"
-        />
+        <input type="text" id="search" />
       </div>
       <div>
         <span>Cart</span>
