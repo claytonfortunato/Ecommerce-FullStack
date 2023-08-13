7b60c15dbd32e35f823ded62dce67675e4580bd4
claytonfortunato <claytonreis10@gmail.com>
2023-08-13 15:09:09 -0300

    feat: header page and icons

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,22 @@
+import { MagnifyingGlass } from "@phosphor-icons/react";
+import moon from "../../assets/icon/moon.svg";
+
 export const Header = () => {
   return (
-    <div className="">
-      <div className="text-3xl font-bold underline bg-slate-400">
-        Ecommerce API
+    <div className="w-full h-14 flex justify-around items-center  bg-slate-600 ">
+      <div className="text-3xl font-bold">Ecommerce API</div>
+      <div className="h-8 flex items-center bg-orange-400 rounded-sm">
+        <input
+          type="text"
+          id="search"
+          placeholder="Pesquisar produto"
+          className="w-80 border-black border p-1 focus:outline-none"
+        />
+        <MagnifyingGlass size={24} className="m-2 cursor-pointer" />
       </div>
-      <div>
-        <label htmlFor="search">Pesquisa</label>
-        <input type="text" id="search" />
-      </div>
-      <div>
-        <span>Cart</span>
+      <div className="flex">
+        <img src={moon} alt="" className="mr-3" />
+        <span className="mr-3">Cart</span>
         <span>Sign In</span>
       </div>
     </div>
