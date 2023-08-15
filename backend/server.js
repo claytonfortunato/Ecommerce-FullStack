f62566925fdfc13d4eeac14799e2ad0217db0f3b
claytonfortunato <claytonreis10@gmail.com>
2023-08-14 21:59:16 -0300

    feat: added cors in backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const connectToDB = require("./src/config/db");
+
 const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"],
+  })
+);
 
 const port = 8000;
 app.listen(port, () => {
