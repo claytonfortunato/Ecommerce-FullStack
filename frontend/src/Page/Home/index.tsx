bc69c10732ba6acbdae0aee2f60afc0e5fd75c0f
claytonfortunato <claytonreis10@gmail.com>
2023-08-16 23:53:36 -0300

    feat: interface and api

--- frontend/src/Page/Home/index.tsx
+++ frontend/src/Page/Home/index.tsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
 import { api } from "../../service/api";
+import { Product } from "../../interface/product";
 
 export const Home = () => {
-  const [product, setProduct] = useState([]);
+  const [product, setProduct] = useState<Product[]>([]);
 
   useEffect(() => {
-    api.get("");
+    api.get("/api/product").then((resp) => {
+      setProduct(resp.data);
+      console.log(resp.data);
+    });
   }, []);
 
   return (
     <div className="">
       <div>
-        <h1 className="text-4xl flex justify-center font-sans">Products</h1>
+        <h1 className=" text-4xl flex justify-center font-sans">Products</h1>
+
+        <div>
+          {product.map((product) => (
+            <div>
+              <h1>{product.name}</h1>
+              <img src={product.image} alt="" />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
