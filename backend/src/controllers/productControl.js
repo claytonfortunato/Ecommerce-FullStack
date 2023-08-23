8262adea579ead34dd7337cda580955e21c9f07c
claytonfortunato <claytonreis10@gmail.com>
2023-08-22 23:53:34 -0300

    feat: created api products

--- backend/src/controllers/productControl.js
+++ backend/src/controllers/productControl.js
@@ -2,9 +2,13 @@ const ProductModel = require("../models/productModel");
 const products = require("../data/Products");
 
 const getProduct = async (req, res) => {
-  // const products = await ProductModel.find();
-  // res.json(products);
-  res.json(products);
+  try {
+    const product = await ProductModel.create(req.body);
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 module.exports = { getProduct };
